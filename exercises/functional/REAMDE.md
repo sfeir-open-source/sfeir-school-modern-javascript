872543173f201f9d33514980fde2af2ade2d75f4
Anthony Goussot <nalunux@gmail.com>
2019-09-30 07:27:10 +0200

    feat(functional): added help for exo 2

--- exercises/functional/REAMDE.md
+++ exercises/functional/REAMDE.md
@@ -1,9 +1,16 @@
 # Fn JS : Exercice
 
-Créer une fonction (getTotalAmount) qui calcul le prix total des livres du panier shoppingCart.
+Créer une fonction (getTotalAmount) qui calcul le prix total des livres du panier shoppingCart
 
 La fonction doit :
 
 1. filtrer par type “books”
 2. transformer l’objet shoppingCart en collection de montants via map
 3. calculer le montant total via reduce
+
+# Fn JS : Exercice 2
+
+Aide pour coder pipe
+
+1. Pipe prend un nombre indéterminé de fonctions en entrée
+2. Pipe est appelé en deux temps : Une première fois pour déclarer ses fonctions, une seconde pour lui passer sa valeur d'entrée
--- exercises/functional/exo.js
+++ exercises/functional/exo.js
@@ -5,23 +5,10 @@ let shoppingCart = [
   { productTitle: "Clean Code", type: "books", amount: 60 }
 ];
 
-/*
-*  Pour coder Pipe
-*  1. Mon pipe prend un nombre indéterminé de fonctions en entrée
-*  2. Mon pipe est appelé en deux temps : Une première fois
-*   pour déclarer ses fonctions, une seconde pour lui passer sa valeur d'entrée
-* */
-var pipe = (...fns) => x => fns.reduce((acc, f) => f(acc), x);
-const filter = fn => value => value.filter(fn)
-const map = fn => value => value.map(fn)
-const reduce = (fn, initial) => value => value.reduce(fn, initial)
+function getTotalAmount() {
+  return /**/
+}
 
-const getTotalAmountViaPipe = pipe(
-  filter(byBooks),
-  map(getAmount),
-  reduce(sumAmount, 0)
-);
+const totalAmount = getTotalAmount(shoppingCart);
 
-const totalAmount = getTotalAmountViaPipe(shoppingCart);
-
-module.exports = { totalAmount } ;
+module.exports = { totalAmount };
