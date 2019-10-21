37e99b59ac07666fe9e80f9aefd960758504ae5a
Tristan ELAIN <tristan.elain@gmail.com>
2019-10-21 13:59:37 +0200

    Exercises App.js into function

--- exercises/redux/exercice/src/App.js
+++ exercises/redux/exercice/src/App.js
@@ -5,23 +5,20 @@ import Todo from "./todo";
 import FetchData from "./fetch";
 import { getState } from "./myRedux";
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <hr />
-          <h1>Counter</h1>
-          <hr />
-          <CounterButtons />
-          <hr />
-          <Text />
-          <h1>Todo</h1>
-          <hr />
-          <Todo />
-          <hr />
-        </div>
-    );
-  }
-}
+const App = () => (
+    <div className="App">
+      <hr />
+      <h1>Counter</h1>
+      <hr />
+      <CounterButtons />
+      <hr />
+      <Text />
+      <h1>Todo</h1>
+      <hr />
+      <Todo />
+      <hr />
+    </div>
+);
+
 
 export default App;
